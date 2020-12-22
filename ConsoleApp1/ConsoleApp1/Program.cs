f4535ce07bfc9864a6b81310be0e62e2780e7e0a
Miquel Roig <miquel.roig@urv.cat>
2020-12-22 13:24:09 +0100

    try1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            int num1 = 5;
+            int num2 = 6;
+
+            double num3 = 2.57;
+            double suma;
+
+            suma = num1 + num3;
+
+            Console.WriteLine(suma);
+            string valor1 = Console.ReadLine();
+            Console.WriteLine("El numnero introduit es: " + valor1);
+            int valorint = Convert.ToInt32(valor1);
+
+            Console.ReadKey();
+        }
+    }
+}
